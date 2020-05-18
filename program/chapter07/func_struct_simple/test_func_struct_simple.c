3b04b0c1ccddca3d62b2c407217b93f0a5a41866
msyksphinz <msyksphinz.dev@gmail.com>
2020-05-18 22:10:02 +0900

    Update Makefile

--- program/chapter07/func_struct_simple/test_func_struct_simple.c
+++ program/chapter07/func_struct_simple/test_func_struct_simple.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "func_struct_simple.h"
 
-// extern int func (struct S elem);
-
 extern int func_S32 (struct S32 elem);
 extern int func_S64 (struct S64 elem);
 extern int func_S128 (struct S128 elem);
@@ -10,25 +8,6 @@ extern int func_S256 (struct S256 elem);
 
 int main()
 {
-  printf("Hello World\n");
-
-  // struct S elem;
-  // for (int i = 0; i < STRUCT_SIZE; i++) {
-  //   elem.x[i] = i + 100;
-  // }
-  // int total = func (elem);
-  //
-  // printf("total = %d\n", total);
-  //
-  // int ref_total = 0;
-  // for (int i = 0; i < STRUCT_SIZE; i++) {
-  //   ref_total += (100 + i);
-  // }
-  //
-  // printf("ref_total = %d\n", ref_total);
-
-
-
   struct S256 s256;
   s256.s128.s64.s32.a = 100;
   s256.s128.s64.s32.b = 200;
