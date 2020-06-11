393184b9174c62d70b38b3922e232e3eda1e5237
msyksphinz <msyksphinz.dev@gmail.com>
2020-06-11 12:35:44 +0000

    Update fp_others

--- program/chapter09/fp_others.c
+++ program/chapter09/fp_others.c
@@ -9,12 +9,12 @@ float f_neg  (float in) { return -in; }
 double d_abs  (double in) { return fabs(in); }
 double d_neg  (double in) { return -in; }
 
-int32_t  cvt_fp_to_sint (float  in) { return static_cast< int32_t>(in); }
-uint32_t cvt_fp_to_uint (float  in) { return static_cast<uint32_t>(in); }
-int32_t  cvt_dp_to_sint (double in) { return static_cast< int32_t>(in); }
-uint32_t cvt_dp_to_uint (double in) { return static_cast<uint32_t>(in); }
+int32_t  cvt_fp_to_sint (float  in) { return ( int32_t)(in); }
+uint32_t cvt_fp_to_uint (float  in) { return (uint32_t)(in); }
+int32_t  cvt_dp_to_sint (double in) { return ( int32_t)(in); }
+uint32_t cvt_dp_to_uint (double in) { return (uint32_t)(in); }
 
-float  cvt_sint_to_fp (int32_t  in) { return static_cast<float >(in); }
-float  cvt_uint_to_fp (uint32_t in) { return static_cast<float >(in); }
-double cvt_sint_to_dp (int32_t  in) { return static_cast<double>(in); }
-double cvt_uint_to_dp (uint32_t in) { return static_cast<double>(in); }
+float  cvt_sint_to_fp (int32_t  in) { return (float )(in); }
+float  cvt_uint_to_fp (uint32_t in) { return (float )(in); }
+double cvt_sint_to_dp (int32_t  in) { return (double)(in); }
+double cvt_uint_to_dp (uint32_t in) { return (double)(in); }
