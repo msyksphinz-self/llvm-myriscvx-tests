261d1ccf3402a08879d74619d39b3241a975aa01
msyksphinz <msyksphinz.dev@gmail.com>
2020-06-12 23:09:05 +0900

    Update chapter08

--- program/chapter09/fp_args.c
+++ program/chapter09/fp_args.c
@@ -1,16 +1,19 @@
-float test_dp_arg(float a, float b, float c)
+float test_fp_arg(float a, float b, float c)
 {
   return a + b + c;
 }
+
 double test_dp_arg(double a, double b, double c)
 {
   return a + b + c;
 }
-float test_dp_longarg(float f0, float f1, float f2, float f3, float f4,
+
+float test_fp_longarg(float f0, float f1, float f2, float f3, float f4,
                       float f5, float f6, float f7, float f8, float f9)
 {
   return f0 + f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8 + f9;
 }
+
 double test_dp_longarg(double f0, double f1, double f2, double f3, double f4,
                        double f5, double f6, double f7, double f8, double f9)
 {
