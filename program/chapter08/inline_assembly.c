21ea9a8bc9d3483e29b8c8bcb50808a9bc926fc9
msyksphinz <msyksphinz.dev@gmail.com>
2020-07-06 23:01:09 +0900

    Update chapter08

--- program/chapter08/inline_assembly.c
+++ program/chapter08/inline_assembly.c
@@ -4,7 +4,7 @@ int inline_assembly(int a)
 {
   int mcycle;
 
-  asm volatile ("csrr %0, %1, zero": "=r"(mcycle): "i"(MCYCLE));
+  asm volatile ("csrrs %0, %1, zero": "=r"(mcycle): "i"(MCYCLE));
   asm volatile ("add  %0,%1,%2":"=r"(mcycle): "r"(mcycle), "r"(a));
 
   return mcycle;
