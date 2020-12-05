30fcc11a1168e6d60caa63a8216a11278d6467f0
msyksphinz <msyksphinz.dev@gmail.com>
2020-12-05 06:47:10 +0000

    Fix compare_slt

--- program/chapter05/compare_slt.c
+++ program/chapter05/compare_slt.c
@@ -1,6 +1,6 @@
-int compare_eq(int a, int b, int c, int d) { return (a == b) ? c : d; }
-int compare_ne(int a, int b, int c, int d) { return (a != b) ? c : d; }
-int compare_lt(int a, int b, int c, int d) { return (a <  b) ? c : d; }
-int compare_le(int a, int b, int c, int d) { return (a <= b) ? c : d; }
-int compare_gt(int a, int b, int c, int d) { return (a >  b) ? c : d; }
-int compare_ge(int a, int b, int c, int d) { return (a >= b) ? c : d; }
+int compare_eq(int a, int b, int c, int d) { return (a == b); }
+int compare_ne(int a, int b, int c, int d) { return (a != b); }
+int compare_lt(int a, int b, int c, int d) { return (a <  b); }
+int compare_le(int a, int b, int c, int d) { return (a <= b); }
+int compare_gt(int a, int b, int c, int d) { return (a >  b); }
+int compare_ge(int a, int b, int c, int d) { return (a >= b); }
